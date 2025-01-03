3bbea24db0f6535b87a0cbdc499f77fc5606ee9c
zshelali <ali.hajeri24@gmail.com>
2025-01-03 16:22:58 +0100

    main

--- turing-machine/src/Main.java
+++ turing-machine/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) throws Exception {
+        CodeSalle codeChallenge1 = new CodeSalle('b', 'a', 2, 1);
+        
+
         System.out.println("Hello, World!");
     }
 }
+                      
