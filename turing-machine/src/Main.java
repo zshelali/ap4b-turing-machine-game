8610d760213910d927b00161f8c416a3a19d74c4
zshelali <ali.hajeri24@gmail.com>
2025-01-03 16:39:12 +0100

    as

--- turing-machine/src/Main.java
+++ turing-machine/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        CodeSalle codeChallenge1 = new CodeSalle('b', 'a', 2, 1);
-        
+        CodeSalle code1 = new CodeSalle('b', 'a', 2, 1);
+        CodeSalle code2 = new CodeSalle('m', 'c', 4, 9);
+        CodeSalle code3 = new CodeSalle('m', 'e', 0, 4);
+
+
 
         System.out.println("Hello, World!");
     }
