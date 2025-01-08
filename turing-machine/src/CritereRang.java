de81889b8161d1e94b3f92c494e400c77df7646b
MouradK2003 <152425002+MouradK2003@users.noreply.github.com>
2025-01-08 15:27:18 +0100

    Update CritereRang.java
    Essai1

--- turing-machine/src/CritereRang.java
+++ turing-machine/src/CritereRang.java
@@ -1,13 +1,19 @@
 /* comparaisons sur le rang des numeros */
 
 public class CritereRang extends Critere {
+    private int etageMin; // Étage minimum
+    private int numMin;   // Numéro de salle minimum
 
-	public void verifEtage() {
+    public CritereRang(int etageMin, int numMin) {
+        this.etageMin = etageMin;
+        this.numMin = numMin;
+    }
 
-	}
+    public boolean verifEtage(CodeSalle codeSalle) {
+        return codeSalle.getEtage() > etageMin; // Vérifie si l'étage est supérieur à etageMin
+    }
 
-	public void verifNum() {
-
-	}
-
+    public boolean verifNum(CodeSalle codeSalle) {
+        return codeSalle.getSalle() >= numMin; // Vérifie si le numéro de salle est supérieur ou égal à numMin
+    }
 }
