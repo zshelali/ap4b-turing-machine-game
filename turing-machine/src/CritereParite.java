c9e4dc6a1ad7701dcd76028aaeb52ebb5ddf2c9f
MouradK2003 <152425002+MouradK2003@users.noreply.github.com>
2025-01-08 15:26:51 +0100

    Update CritereParite.java
    Essai1

--- turing-machine/src/CritereParite.java
+++ turing-machine/src/CritereParite.java
@@ -1,13 +1,11 @@
 /* comparaisons sur la parite */
 
 public class CritereParite extends Critere {
+    public boolean verifEtage(CodeSalle codeSalle) {
+        return codeSalle.getEtage() % 2 == 0; // Vérifie si l'étage est pair
+    }
 
-	public void verifEtage() {
-
-	}
-
-	public void verifNum() {
-
-	}
-
+    public boolean verifNum(CodeSalle codeSalle) {
+        return codeSalle.getSalle() % 2 == 0; // Vérifie si le numéro de salle est pair
+    }
 }
