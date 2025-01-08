5bfdf343f9b6e647c161153ce07a44a904c9a957
MouradK2003 <152425002+MouradK2003@users.noreply.github.com>
2025-01-08 15:26:19 +0100

    Update CritereAlpha.java
    Essai1

--- turing-machine/src/CritereAlpha.java
+++ turing-machine/src/CritereAlpha.java
@@ -1,13 +1,19 @@
 /* criteres par ordre alphabetique */
 
 public class CritereAlpha extends Critere {
+    private String campusRef; // Référence de campus pour la comparaison
+    private String batimentRef; // Référence de bâtiment pour la comparaison
 
-	public void verifCamp(CodeSalle c) {
-		
-	}
+    public CritereAlpha(String campusRef, String batimentRef) {
+        this.campusRef = campusRef;
+        this.batimentRef = batimentRef;
+    }
 
-	public void verifBat(CodeSalle c) {
+    public boolean verifCamp(CodeSalle codeSalle) {
+        return codeSalle.getCampus().compareTo(campusRef) > 0; // Vérifie si le campus est après campusRef dans l'ordre alphabétique
+    }
 
-	}
-
+    public boolean verifBat(CodeSalle codeSalle) {
+        return codeSalle.getBatiment().compareTo(batimentRef) > 0; // Vérifie si le bâtiment est après batimentRef dans l'ordre alphabétique
+    }
 }
