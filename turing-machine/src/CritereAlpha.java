15bd6aa355522dfd993def8ce3d237b93a760b30
MouradK2003 <152425002+MouradK2003@users.noreply.github.com>
2025-01-11 16:14:58 +0100

    Update CritereAlpha.java

--- turing-machine/src/CritereAlpha.java
+++ turing-machine/src/CritereAlpha.java
@@ -1,6 +1,4 @@
 public class CritereAlpha extends Critere {
-    private char campusRef; // Référence de campus pour la comparaison
-    private char batimentRef; // Référence de bâtiment pour la comparaison
 
     public CritereAlpha(String description) {
         super(description);
@@ -8,20 +6,12 @@ public class CritereAlpha extends Critere {
 
     @Override
     public boolean verifier(CodeSalle solution, CodeSalle proposition) {
-        // Vérifie si le bâtiment est avant/après le campus par ordre alphabétique
-        return proposition.getBatiment() < solution.getCampus();
-    }
-    public CritereAlpha(char campusRef, char batimentRef) {
-        this.campusRef = campusRef;
-        this.batimentRef = batimentRef;
+        // Comparaison de l'ordre alphabétique entre le bâtiment et le campus
+        boolean isBatiAvantCampusSolution = solution.getBatiment() < solution.getCampus();
+        boolean isBatiAvantCampusProposition = proposition.getBatiment() < proposition.getCampus();
+    
+        // Vérifie si l'ordre de la proposition correspond à l'ordre de la solution
+        return isBatiAvantCampusSolution == isBatiAvantCampusProposition;
     }
     
-
-    public boolean verifCamp(CodeSalle codeSalle) {
-        return codeSalle.getCampus() > campusRef ; // Vérifie si le campus est après campusRef dans l'ordre alphabétique
-    }
-
-    public boolean verifBat(CodeSalle codeSalle) {
-        return codeSalle.getBatiment() > batimentRef; // Vérifie si le bâtiment est après batimentRef dans l'ordre alphabétique
-    }
 }
