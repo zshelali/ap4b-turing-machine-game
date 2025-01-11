5b8abf1e015a90ff79f4c46307b602c5c76f174c
MouradK2003 <152425002+MouradK2003@users.noreply.github.com>
2025-01-11 16:14:23 +0100

    Update Challenge.java

--- turing-machine/src/Challenge.java
+++ turing-machine/src/Challenge.java
@@ -25,38 +25,50 @@ public class Challenge {
         return critere.verifier(proposition, solution);
     }
 
-     public static Challenge creerChallenge1() {
-        // Challenge 1
+    public static Challenge creerChallenge1() {
+        // Solution du challenge 1
         CodeSalle solution1 = new CodeSalle('B', 'C', 2, 5);
+        
+        // Définition des critères
         List<Critere> criteres1 = List.of(
-            new CritereAlpha("Le bâtiment est avant le campus par ordre alphabétique."),
-            new CritereParite("L'étage est pair.", true)
+            new CritereAlpha("Le bâtiment est avant | après le campus par ordre alphabétique"),
+            new CritereParite("L'étage est pair | impair", true),
+            new CritereSommePaire("La somme des chiffres est paire | impaire", true),
+            new CriterePresenceChiffre("Le nombre d'occurrences du chiffre 4 est identique", '4') 
         );
+        
+        // Création du challenge
         Challenge challenge1 = new Challenge(solution1, criteres1);
-
         return challenge1;
-     }
-        public static Challenge creerChallenge2() {
+    }
+    
+    public static Challenge creerChallenge2() {
         // Challenge 2
         CodeSalle solution2 = new CodeSalle('M', 'A', 3, 7);
         List<Critere> criteres2 = List.of(
-            new CritereAlpha("Le bâtiment est après le campus par ordre alphabétique."),
-            new CritereRang("Le numéro de salle est supérieur à 5.", 5, true)
+            new CritereAlpha("Le bâtiment est avant | après le campus par ordre alphabétique"),
+            new CritereRang("Le numéro de salle est supérieur | inférieur à 5", 5, true),
+            new CritereParite("L'étage est pair | impair", true), 
+            new CriterePresenceChiffre("La présence du chiffre 7 dans l'étage ou le numéro de salle", '7')  
         );
         Challenge challenge2 = new Challenge(solution2, criteres2);
         return challenge2;
     }
-        public static Challenge creerChallenge3(){
+    
+    public static Challenge creerChallenge3() {
         // Challenge 3
-        CodeSalle solution3 = new CodeSalle('S', 'D', 0, 1);
+        CodeSalle solution3 = new CodeSalle('S', 'D', 0, 2);
         List<Critere> criteres3 = List.of(
-            new CritereParite("Le numéro de salle est impair.", false),
-            new CritereRang("L'étage est inférieur à 3.", 3, false)
+            new CritereParite("Le numéro de salle est pair | impair", false),
+            new CritereRang("L'étage est inférieur | supérieur à 3.", 3, false),
+            new CritereAlpha("Le bâtiment est avant | après le campus par ordre alphabétique"),  
+            new CriterePresenceChiffre("La présence du chiffre 2 dans l'étage ou le numéro de salle", '2')  
         );
         Challenge challenge3 = new Challenge(solution3, criteres3);
         return challenge3;
     }
-
+    
+    
         
 }
 
